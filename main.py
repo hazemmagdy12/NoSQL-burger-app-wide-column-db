17756f37ab848687a604201fe850d3d2acedf78c
hazem magdy <ihazemmi30@gmail.com>
2026-09-21 23:40:29 +0300

    Implement full CRUD for user activity in main.py
    Added full CRUD operations for user activity, including update and delete functionality.

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 import time
-from crud import setup_table, log_user_activity, get_user_history
+from crud import setup_table, log_user_activity, get_user_history, update_user_activity, delete_user_activity
 
 def main():
-    print("Starting Phase 3: Wide-Column DB Operations...\n")
+    print(" Starting Phase 3: Wide-Column DB Operations...\n")
     
     table = setup_table()
     
     print("\n--- Simulating User Activity ---")
     log_user_activity(table, user_id="user_123", action="view_item", burger_id="b1")
-    
-    time.sleep(1)
-    
+    time.sleep(1) 
     log_user_activity(table, user_id="user_123", action="add_to_cart", burger_id="b1")
     
     print("\n--- Displaying User History ---")
-    get_user_history(table, user_id="user_123")
+    cursor = table.find({"user_id": "user_123"})
+    activities = list(cursor) 
     
-    print("\n Phase 3 completed successfully!")
+    for row in activities:
+        print(f"- Time: {row['activity_time']}, Action: {row['action']}, Item: {row['burger_id']}")
+    
+    
+    if len(activities) > 0:
+        oldest_activity_time = activities[-1]['activity_time']
+        
+        print("\n--- Updating Activity ---")
+
+        update_user_activity(table, "user_123", oldest_activity_time, "view_item_and_liked")
+        
+        print("\n--- Deleting Activity ---")
+
+        newest_activity_time = activities[0]['activity_time']
+        delete_user_activity(table, "user_123", newest_activity_time)
+        
+        print("\n--- History After Update & Delete ---")
+        get_user_history(table, "user_123")
+
+    print("\n Phase 3 Full CRUD completed successfully!")
 
 if __name__ == "__main__":
     main()
